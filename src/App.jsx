48b54ffc05bcf1f55f8a76779b3f39ad1c5bb28d
Thomas Daley <prayerproduction@gmail.com>
2023-02-18 16:25:59 +0000

    added is valid website constructor and check

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Bookmarks from "./components/Bookmarks";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Bookmarks />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bookmarks.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import isValidHttpUrl from "../utils/functions";
+
+const Bookmarks = () => {
+  const [bookmarks, setBookmarks] = useState(() => {
+    const savedBookmarks = localStorage.getItem("bookmarks");
+
+    if (savedBookmarks) {
+      return JSON.parse(savedBookmarks);
+    } else {
+      return [];
+    }
+  });
+
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }, [bookmarks]);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (inputValue.length) {
+      if (isValidHttpUrl(inputValue)) {
+        setBookmarks((prevBookmarks) => [
+          ...prevBookmarks,
+          {
+            id: bookmarks.length + 1,
+            text: inputValue,
+          },
+        ]);
+      }
+    }
+  };
+
+  const handleDeleteClick = (id) => {
+    const filteredBookmarks = bookmarks.filter((bookmark) => bookmark.id != id);
+    setBookmarks(filteredBookmarks);
+  };
+
+  return (
+    <div>
+      <section>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            placeholder="Add a Bookmark"
+            value={inputValue}
+            onChange={(event) => {
+              setInputValue(event.target.value);
+            }}
+            type="text"
+          />
+
+          <button>Add</button>
+        </form>
+
+        <div className="bookmarks-list">
+          {bookmarks.map((bookmark) => {
+            return (
+              <div key={bookmark.id} className="single-bookmark">
+                <a href={bookmark.text}>{bookmark.text}</a>
+                <button onClick={() => handleDeleteClick(bookmark.id)}>
+                  X
+                </button>
+              </div>
+            );
+          })}
+        </div>
+
+        <button
+          onClick={() => {
+            setBookmarks([]);
+          }}
+        >
+          Clear Bookmarks
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default Bookmarks;
